43e975f9ea4917da9794d0aa774373dc75b8c9d7
zentia <363476658@qq.com>
2025-04-03 18:01:55 +0800

    xmake

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,19 @@
 target("imgui")
     set_kind("$(kind)")
     add_includedirs("./", {public = true})
-    add_headerfiles("imgui.h")
+    add_includedirs("backends", {public = true})
+    add_headerfiles(
+        "imgui.h", 
+        "backends/imgui_impl_vulkan.h", 
+        "backends/imgui_impl_glfw.h"
+    )
     add_files(
         "imgui.cpp", 
         "imgui_draw.cpp", 
         "imgui_widgets.cpp",
-        "imgui_tables.cpp"
+        "imgui_tables.cpp",
+        "backends/imgui_impl_vulkan.cpp",
+        "backends/imgui_impl_glfw.cpp"
     )
+    add_packages("vulkansdk")
+    add_deps("glfw")
